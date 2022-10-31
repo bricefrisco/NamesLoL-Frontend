c062e2912435a0239dd6c31d2e689ff07b6707ef
Brice Frisco <39070938+bricefrisco@users.noreply.github.com>
2022-10-31 07:08:23 -0500

    (Experimental) mobile permaloading fix

--- pages/lol-name-checker.js
+++ pages/lol-name-checker.js
@@ -67,21 +67,18 @@ const LoLNameChecker = ({
   }, [error]);
 
   useEffect(() => {
+    if (loading) {
+      setLoading(false);
+    }
+
     const handleStart = (url) =>
       url.startsWith("/lol-name-checker?") &&
       url !== router.asPath &&
       setLoading(true);
 
-    const handleComplete = (url) => url === router.asPath && setLoading(false);
-
     router.events.on("routeChangeStart", handleStart);
-    router.events.on("routeChangeComplete", handleComplete);
-    router.events.on("routeChangeError", handleComplete);
-
     return () => {
       router.events.off("routeChangeStart", handleStart);
-      router.events.off("routeChangeComplete", handleComplete);
-      router.events.off("routeChangeError", handleComplete);
     };
   }, [router]);
 
